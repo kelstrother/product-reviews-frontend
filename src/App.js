7916066fcd24bb77ada46fa13da3f747930118a0
Kel Strother <kelstrother@gmail.com>
2021-02-24 21:29:01 -0700

    Commit 10: AJAX request to Products Index

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 function App() {
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const makeAPICall = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/products")
+        const json = await res.json()
+        console.log('this is json', json)
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    makeAPICall()
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing</h1>
     </div>
   );
 }
